6752500a3173628829c724eead993882c5bc4c7d
Don Jayamanne <don.jayamanne@outlook.com>
2024-06-05 17:33:23 +1000

    Create hnew envs

--- crates/pet-conda/tests/ci_test.rs
+++ crates/pet-conda/tests/ci_test.rs
@@ -8,7 +8,7 @@ mod common;
 #[cfg(unix)]
 #[cfg_attr(feature = "ci", test)]
 #[allow(dead_code)]
-fn conda_root_ci() {
+fn detect_conda_root() {
     use pet_conda::Conda;
     use pet_core::{
         manager::EnvManagerType, os_environment::EnvironmentApi,
@@ -32,12 +32,107 @@ fn conda_root_ci() {
     assert_eq!(manager.version, info.conda_version.into());
 
     let env = &result.environments[0];
-    assert_eq!(env.prefix, PathBuf::from(info.conda_prefix.clone()).into());
-    assert_eq!(env.version.is_some(), true);
+    assert_eq!(env.prefix, conda_dir.clone().into());
     assert_eq!(env.name, Some("base".into()));
     assert_eq!(env.category, PythonEnvironmentCategory::Conda);
     assert_eq!(env.executable, Some(conda_dir.join("bin").join("python")));
     assert_eq!(env.version, Some(get_version(&info.python_version)));
+
+    assert_eq!(env.manager, Some(manager.clone()));
+}
+
+#[cfg(unix)]
+#[cfg_attr(feature = "ci", test)]
+#[allow(dead_code)]
+fn detect_new_conda_env() {
+    use pet_conda::Conda;
+    use pet_core::{
+        os_environment::EnvironmentApi, python_environment::PythonEnvironmentCategory, Locator,
+    };
+    use std::path::PathBuf;
+
+    let env_name = "env_with_python";
+    create_conda_env_with_python(&env_name);
+    let env = EnvironmentApi::new();
+
+    let conda = Conda::from(&env);
+    let result = conda.find().unwrap();
+    println!("After creating Env {:?}", result);
+
+    assert_eq!(result.managers.len(), 1);
+
+    let manager = &result.managers[0];
+
+    let info = get_conda_info();
+    let conda_dir = PathBuf::from(info.conda_prefix.clone());
+    let env = result
+        .environments
+        .iter()
+        .find(|x| x.name == Some(env_name.into()))
+        .expect(
+            format!(
+                "New Environment not created, detected envs {:?}",
+                result.environments
+            )
+            .as_str(),
+        );
+
+    let prefix = conda_dir.clone().join("envs").join(env_name);
+    assert_eq!(env.prefix, prefix.clone().into());
+    assert_eq!(env.name, Some(env_name.into()));
+    assert_eq!(env.category, PythonEnvironmentCategory::Conda);
+    assert_eq!(env.executable, prefix.join("bin").join("python").into());
+    assert!(
+        env.version.clone().unwrap_or_default().starts_with("3.10"),
+        "Expected 3.10, but got Version: {:?}",
+        env.version
+    );
+
+    assert_eq!(env.manager, Some(manager.clone()));
+}
+
+#[cfg(unix)]
+#[cfg_attr(feature = "ci", test)]
+#[allow(dead_code)]
+fn detect_new_conda_env_without_python() {
+    use pet_conda::Conda;
+    use pet_core::{
+        os_environment::EnvironmentApi, python_environment::PythonEnvironmentCategory, Locator,
+    };
+    use std::path::PathBuf;
+
+    let env_name = "env_without_python";
+    create_conda_env_without_python(&env_name);
+    let env = EnvironmentApi::new();
+
+    let conda = Conda::from(&env);
+    let result = conda.find().unwrap();
+    println!("After creating Env {:?}", result);
+
+    assert_eq!(result.managers.len(), 1);
+
+    let manager = &result.managers[0];
+
+    let info = get_conda_info();
+    let conda_dir = PathBuf::from(info.conda_prefix.clone());
+    let env = result
+        .environments
+        .iter()
+        .find(|x| x.name == Some(env_name.into()))
+        .expect(
+            format!(
+                "New Environment not created, detected envs {:?}",
+                result.environments
+            )
+            .as_str(),
+        );
+
+    let prefix = conda_dir.clone().join("envs").join(env_name);
+    assert_eq!(env.prefix, prefix.clone().into());
+    assert_eq!(env.name, Some(env_name.into()));
+    assert_eq!(env.category, PythonEnvironmentCategory::Conda);
+    assert_eq!(env.executable.is_none(), true);
+    assert_eq!(env.version.is_none(), true);
 
     assert_eq!(env.manager, Some(manager.clone()));
 }
@@ -51,17 +146,36 @@ struct CondaInfo {
     envs: Vec<String>,
 }
 
-fn get_conda_info() -> CondaInfo {
+fn get_conda_exe() -> &'static str {
     // On CI we expect conda to be in the current path.
-    let conda_exe = "conda";
+    "conda"
+}
+
+fn get_conda_info() -> CondaInfo {
     // Spawn `conda --version` to get the version of conda as a string
-    let output = std::process::Command::new(conda_exe)
+    let output = std::process::Command::new(get_conda_exe())
         .args(["info", "--json"])
         .output()
         .expect("Failed to execute command");
     let output = String::from_utf8(output.stdout).unwrap();
     let conda_info: CondaInfo = serde_json::from_str(&output).unwrap();
     conda_info
+}
+
+fn create_conda_env_with_python(name: &str) {
+    // Spawn `conda --version` to get the version of conda as a string
+    let _ = std::process::Command::new(get_conda_exe())
+        .args(["create", "-n", name, "python=3.10", "-y"])
+        .output()
+        .expect("Failed to execute command");
+}
+
+fn create_conda_env_without_python(name: &str) {
+    // Spawn `conda --version` to get the version of conda as a string
+    let _ = std::process::Command::new(get_conda_exe())
+        .args(["create", "-n", name, "-y"])
+        .output()
+        .expect("Failed to execute command");
 }
 
 fn get_version(value: &String) -> String {
