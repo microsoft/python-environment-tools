8096ac980528ef9ef828a3f43366312df63f285d
Don Jayamanne <don.jayamanne@outlook.com>
2024-06-05 22:04:15 +1000

    oops

--- crates/pet-windows-store/src/environments.rs
+++ crates/pet-windows-store/src/environments.rs
@@ -49,7 +49,7 @@ impl PotentialPython {
         let exe = self.exe.clone().unwrap_or_default();
         let parent = path.parent()?.to_path_buf(); // This dir definitely exists.
         if let Some(result) = get_package_display_name_and_location(&name, hkcu) {
-            let env_path = fix_file_path_casing(PathBuf::from(result.env_path));
+            let env_path = fix_file_path_casing(&PathBuf::from(result.env_path));
 
             Some(
                 PythonEnvironmentBuilder::new(
