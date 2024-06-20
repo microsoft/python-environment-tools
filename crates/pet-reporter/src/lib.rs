d7a203d9700a4002728bf39823f3cb8628fa4d11
Don Jayamanne <don.jayamanne@outlook.com>
2024-06-21 06:58:53 +1000

    Fixes

--- crates/pet-reporter/src/lib.rs
+++ crates/pet-reporter/src/lib.rs
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+pub mod cache;
 pub mod environment;
 pub mod jsonrpc;
 pub mod manager;
 pub mod stdio;
 pub mod test;
-pub mod cache;
