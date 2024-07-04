b14792d0b3f8ba7805d2ba59a19c7824933b0a1f
Christian Clauss <cclauss@me.com>
2024-07-04 10:33:56 +0200

    interpreterInfo.py: Create the object in a single operation

--- crates/pet/tests/interpreterInfo.py
+++ crates/pet/tests/interpreterInfo.py
@@ -4,12 +4,13 @@
 import json
 import sys
 
-obj = {}
-obj["version_info"] = tuple(sys.version_info)
-obj["sys_prefix"] = sys.prefix
-obj["sys_version"] = sys.version
-obj["is64_bit"] = sys.maxsize > 2**32
-obj["executable"] = sys.executable
+obj = {
+    "version_info": tuple(sys.version_info),
+    "sys_prefix": sys.prefix,
+    "sys_version": sys.version,
+    "is64_bit": sys.maxsize > 2**32,
+    "executable": sys.executable,
+}
 
 # Everything after this is the information we need
 print("503bebe7-c838-4cea-a1bc-0f2963bcb657")
