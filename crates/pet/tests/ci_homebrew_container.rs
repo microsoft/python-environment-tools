f6c944065942931f22151f0621042e38fc620c0e
Don Jayamanne <don.jayamanne@outlook.com>
2024-08-21 23:08:48 +1000

    Fix tests

--- crates/pet/tests/ci_homebrew_container.rs
+++ crates/pet/tests/ci_homebrew_container.rs
@@ -52,6 +52,8 @@ fn verify_python_in_homebrew_contaner() {
         executable: Some(PathBuf::from("/home/linuxbrew/.linuxbrew/bin/python3")),
         version: Some("3.12.4".to_string()), // This can change on CI, so we don't check it
         symlinks: Some(vec![
+            PathBuf::from("/home/linuxbrew/.linuxbrew/Cellar/python@3.12/3.12.5/bin/python3"),
+            PathBuf::from("/home/linuxbrew/.linuxbrew/Cellar/python@3.12/3.12.5/bin/python3.12"),
             PathBuf::from("/home/linuxbrew/.linuxbrew/bin/python3"),
             PathBuf::from("/home/linuxbrew/.linuxbrew/bin/python3.12"),
             PathBuf::from("/home/linuxbrew/.linuxbrew/opt/python3/bin/python3"),
@@ -60,8 +62,6 @@ fn verify_python_in_homebrew_contaner() {
             PathBuf::from("/home/linuxbrew/.linuxbrew/opt/python@3/bin/python3.12"),
             PathBuf::from("/home/linuxbrew/.linuxbrew/opt/python@3.12/bin/python3"),
             PathBuf::from("/home/linuxbrew/.linuxbrew/opt/python@3.12/bin/python3.12"),
-            PathBuf::from("/home/linuxbrew/.linuxbrew/Cellar/python@3.12/3.12.5/bin/python3"),
-            PathBuf::from("/home/linuxbrew/.linuxbrew/Cellar/python@3.12/3.12.5/bin/python3.12"),
             // On CI the Python version can change with minor updates, so we don't check the full version.
             // PathBuf::from("/home/linuxbrew/.linuxbrew/Cellar/python@3.12/3.12.4/bin/python3.12"),
         ]),
